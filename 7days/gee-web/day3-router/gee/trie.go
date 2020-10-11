014045ad0f36add5e7a0f0bc9ccc2a26c88fa6fb
tiantian <1150976163@qq.com>
2020-10-11 22:51:32 +0800

    7天造轮子系列-模仿gin-router

--- 7days/gee-web/day3-router/gee/trie.go
+++ 7days/gee-web/day3-router/gee/trie.go
@@ -1,12 +1,15 @@
 package gee
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type node struct {
 	pattern  string  //待匹配路由
 	part     string  //路由中的一部分
 	children []*node //子节点
-	isWild   bool    //是否是精确匹配
+	isWild   bool    //是否是精确匹配 part含有:或*时为true
 }
 
 func (n *node) String() string {
@@ -19,4 +22,64 @@ func (n *node) insert(pattern string, parts []string, height int) {
 		return
 	}
 
+	part := parts[height]
+	child := n.matchChild(part)
+
+	if child == nil {
+		child = &node{
+			part:   part,
+			isWild: part[0] == ':' || part[0] == '*',
+		}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
+
+func (n *node) travel(list *([]*node)) {
+	if n.pattern != "" {
+		*list = append(*list, n)
+	}
+	for _, child := range n.children {
+		child.travel(list)
+	}
+}
+
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+
+	return nodes
 }
