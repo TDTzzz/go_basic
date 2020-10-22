f432084e289abc3d34535f33af1ba142df3d4b3e
tiantian <1150976163@qq.com>
2020-10-22 23:13:39 +0800

    从零开始rpc-day2-支持并发与异步的客户端

---
+++ 7days/gee-rpc/day2-client/main/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	day2_client "go_basic/7days/gee-rpc/day2-client"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func startServer(addr chan string) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	day2_client.Accept(l)
+}
+
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := day2_client.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("rpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
+	}
+	wg.Wait()
+}
