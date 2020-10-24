b44fcd864e8629ac18ccc447adde23683342114d
tiantian <1150976163@qq.com>
2020-10-24 18:42:25 +0800

    basic系列-reflect反射

--- basic/reflect.go
+++ basic/reflect.go
@@ -1,15 +1,84 @@
 package main
 
+//研究反射
+
 import (
-	"log"
+	"fmt"
 	"reflect"
 )
 
-func main() {
-	var a int = 50
-	v := reflect.ValueOf(a)
-	t := reflect.TypeOf(a)
-	k := reflect.Kind(a)
+type Foo struct {
+	Name string
+	Age  int
+	sex  string
+}
 
-	log.Println(v, t, k)
+func (foo Foo) Test() {
+	fmt.Println("----")
+}
+
+func main() {
+	var foo Foo
+	PrintValue(reflect.ValueOf(foo))
+}
+
+func PrintValue(v reflect.Value) {
+	fmt.Println("--------------------")
+	// ----- 通用方法 -----
+	fmt.Println("String             :", v.String())  // 反射值的字符串形式
+	fmt.Println("Type               :", v.Type())    // 反射值的类型
+	fmt.Println("Kind               :", v.Kind())    // 反射值的类别
+	fmt.Println("CanAddr            :", v.CanAddr()) // 是否可以获取地址
+	fmt.Println("CanSet             :", v.CanSet())  // 是否可以修改
+	if v.CanAddr() {
+		fmt.Println("Addr               :", v.Addr())       // 获取地址
+		fmt.Println("UnsafeAddr         :", v.UnsafeAddr()) // 获取自由地址
+	}
+	// 获取方法数量
+	fmt.Println("NumMethod(私有方法不算)          :", v.NumMethod())
+	if v.NumMethod() > 0 {
+		// 遍历方法
+		i := 0
+		for ; i < v.NumMethod()-1; i++ {
+			fmt.Printf("    ┣ %v\n", v.Method(i).String())
+			//			if i >= 4 { // 只列举 5 个
+			//				fmt.Println("    ┗ ...")
+			//				break
+			//			}
+		}
+		fmt.Printf("    ┗ %v\n", v.Method(i).String())
+		// 通过名称获取方法
+		fmt.Println("MethodByName       :", v.MethodByName("String").String())
+	}
+
+	switch v.Kind() {
+	// 结构体：
+	case reflect.Struct:
+		fmt.Println("=== 结构体 ===")
+		// 获取字段个数
+		fmt.Println("NumField           :", v.NumField())
+		if v.NumField() > 0 {
+			var i int
+			// 遍历结构体字段
+			for i = 0; i < v.NumField()-1; i++ {
+				field := v.Field(i) // 获取结构体字段
+				fmt.Printf("    ├ %-8v %v\n", field.Type(), field.String())
+			}
+			field := v.Field(i) // 获取结构体字段
+			fmt.Printf("    └ %-8v %v\n", field.Type(), field.String())
+			// 通过名称查找字段
+			if v := v.FieldByName("ptr"); v.IsValid() {
+				fmt.Println("FieldByName(ptr)   :", v.Type().Name())
+			}
+			// 通过函数查找字段
+			v := v.FieldByNameFunc(func(s string) bool { return len(s) > 3 })
+			if v.IsValid() {
+				fmt.Println("FieldByNameFunc    :", v.Type().Name())
+			}
+		}
+	}
+}
+
+func PrintType(typ reflect.Type) {
+
 }
