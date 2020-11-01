d5044cbf88f396a2d5ee67d48d7d5acbc55fb86b
tiantian <1150976163@qq.com>
2020-11-01 14:44:35 +0800

    算法-dp-最大上升子序列

--- alg/dp/300-最长上升子序列.go
+++ alg/dp/300-最长上升子序列.go
@@ -22,6 +22,36 @@ func lengthOfLISV1(nums []int) int {
 	return res
 }
 
+//贪心+二分
+func lengthOfLISV2(nums []int) int {
+	len, n := 1, len(nums)
+	if n == 0 {
+		return 0
+	}
+	d := make([]int, n+1)
+	d[len] = nums[0]
+
+	for i := 1; i < n; i++ {
+		if nums[i] > d[len] {
+			len++
+			d[len] = nums[i]
+		} else {
+			l, r, pos := 1, len, 0
+			for l <= r {
+				mid := (l + r) >> 1
+				if d[mid] < nums[i] {
+					pos = mid
+					l = mid + 1
+				} else {
+					r = mid - 1
+				}
+			}
+			d[pos+1] = nums[i]
+		}
+	}
+	return len
+}
+
 func max(i, j int) int {
 	if i > j {
 		return i
@@ -30,7 +60,8 @@ func max(i, j int) int {
 }
 
 func main() {
+
 	nums := []int{10, 9, 2, 5, 3, 7, 101, 18}
-	res := lengthOfLISV1(nums)
+	res := lengthOfLISV2(nums)
 	log.Println(res)
 }
