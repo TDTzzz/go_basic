e725d3763bc651d4a9fd18c389920aa20285940c
tiantian <1150976163@qq.com>
2020-11-09 23:22:03 +0800

    算法-hot-二叉搜索树

---
+++ alg/hot/98-验证二叉搜索树.go
@@ -0,0 +1,46 @@
+package hot
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	return helper(root, math.MinInt64, math.MaxInt64)
+}
+
+func helper(root *TreeNode, lower, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
+}
+
+//方法二：中序遍历
+func isValidBSTV2(root *TreeNode) bool {
+	stack := []*TreeNode{}
+	inorder := math.MinInt64
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if inorder >= root.Val {
+			return false
+		}
+
+		inorder = root.Val
+		root = root.Right
+	}
+
+	return true
+}
