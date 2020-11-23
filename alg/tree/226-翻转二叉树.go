379d0f78208de18560188c997a67275071b340ce
tiantian <1150976163@qq.com>
2020-11-23 22:43:33 +0800

    alg-tree-翻转二叉树和

---
+++ alg/tree/226-翻转二叉树.go
@@ -0,0 +1,22 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//递归
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return root
+}
