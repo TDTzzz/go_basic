31c7ab7fe8b82135c6c63d8b0c0cd3c037cbf1fb
tiantian <1150976163@qq.com>
2020-12-02 21:15:38 +0800

    alg-tree-数的子集

---
+++ alg/tree/572-另一个树的子树.go
@@ -0,0 +1,33 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//V1:DFS暴力匹配
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return false
+	}
+	return check(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t)
+}
+
+func check(s, t *TreeNode) bool {
+	if s == nil && t == nil {
+		return true
+	}
+	if s == nil || t == nil {
+		return false
+	}
+	if s.Val == t.Val {
+		return check(s.Left, t.Left) && check(s.Right, t.Right)
+	}
+	return false
+}
+
+//V2
