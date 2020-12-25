f23406486ffac13b9db84192c253f58165f5a775
tiantian <1150976163@qq.com>
2020-12-25 23:02:57 +0800

    alg-回溯-全排列

---
+++ alg/classic/46-全排列.go
@@ -0,0 +1,52 @@
+package main
+
+func permute(nums []int) [][]int {
+	if len(nums) == 1 {
+		return [][]int{nums}
+	}
+
+	res := [][]int{}
+
+	for i, num := range nums {
+		// 把num从 nums 拿出去 得到tmp
+		tmp := make([]int, len(nums)-1)
+		copy(tmp[0:], nums[0:i])
+		copy(tmp[i:], nums[i+1:])
+
+		// sub 是把num 拿出去后，数组中剩余数据的全排列
+		sub := permute(tmp)
+		for _, s := range sub {
+			res = append(res, append(s, num))
+		}
+	}
+	return res
+}
+
+//dfs+回溯
+func permuteV2(nums []int) [][]int {
+	res := [][]int{}
+	visited := map[int]bool{}
+
+	var dfs func(path []int)
+	dfs = func(path []int) {
+		if len(path) == len(nums) {
+			temp := make([]int, len(path))
+			copy(temp, path)
+			res = append(res, temp)
+			return
+		}
+		for _, n := range nums {
+			if visited[n] {
+				continue
+			}
+			path = append(path, n)
+			visited[n] = true
+			dfs(path)
+			path = path[:len(path)-1]
+			visited[n] = false
+		}
+	}
+
+	dfs([]int{})
+	return res
+}
