d62fa9679965efaae5d5af80937d945694daf658
tiantian <1150976163@qq.com>
2020-12-29 23:20:23 +0800

    alg-滑动窗口

---
+++ alg/slidingwindow/最长无重复子串.go
@@ -0,0 +1,21 @@
+package main
+
+func lengthOfLongestSubstring(s string) (res int) {
+	window := make(map[string]int)
+	left, right := 0, 0
+	
+	for right < len(s) {
+		c := string(s[right])
+		right++
+		window[c]++
+		for window[c] > 1 {
+			d := string(s[left])
+			left++
+			window[d]--
+		}
+		if res < right-left {
+			res = right - left
+		}
+	}
+	return
+}
